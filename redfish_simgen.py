e980d1c51343ae497be9843d3522aaa71d5a8b2e
michzimm <michzimm@cisco.com>
2022-10-24 15:34:37 -0400

    initial commit

--- redfish_simgen.py
+++ redfish_simgen.py
@@ -5,6 +5,8 @@ import string
 import shutil
 import os
 import fnmatch
+import docker
+
 
 # Functions
 def findReplace(directory, find, replace, filePattern):
@@ -17,31 +19,42 @@ def findReplace(directory, find, replace, filePattern):
             with open(filepath, "w") as f:
                 f.write(s)
 
+#Get inputs
+sim_type = input("Please select simulator type [hp, dell]: ")
+port_num = input("Please enter a port number to run the simulator on between 30000-30999: ")
 
-# Generate random serial number
-serial = str(''.join(random.choices(string.ascii_uppercase + string.digits, k=10)))
+if sim_type == 'hp':
 
-# Generate hostname
-hostname = "ILO"+serial
+    # Generate random serial number
+    serial = str(''.join(random.choices(string.ascii_uppercase + string.digits, k=10)))
 
-# Generate server_name
-server_name = serial+".cisco.lab"
+    # Generate hostname
+    hostname = "ILO"+serial
 
-# Generate random IP address
-ip = "10."+(".".join(str(random.randint(0, 255)) for _ in range(3)))
+    # Generate server_name
+    server_name = serial+".cisco.lab"
 
-print("Serial Number: "+serial)
-print ("ILO Hostname: "+hostname)
-print ("Server Name: "+server_name)
-print ("IP Address: "+ip)
+    # Generate random IP address
+    ip = "10."+(".".join(str(random.randint(0, 255)) for _ in range(3)))
 
-# Create new directory tree for server, copy from template
-source_dir = "./hp_server.template"
-dest_dir = "./instances/hp_server."+serial
-shutil.copytree(source_dir, dest_dir)
+    print("Serial Number: "+serial)
+    print ("ILO Hostname: "+hostname)
+    print ("Server Name: "+server_name)
+    print ("IP Address: "+ip)
 
-# Replace unique identifiers (serial, hostname, server_name, ip) in server directory tree
-findReplace("./hp_server_test", "{{SERIAL_NUMBER}}", serial, "*")
-findReplace("./hp_server_test", "{{ILO_HOSTNAME}}", hostname, "*")
-findReplace("./hp_server_test", "{{SERVER_NAME}}", server_name, "*")
-findReplace("./hp_server_test", "{{IP_ADDRESS}}", ip, "*")
+    # Generate instance name
+    instance_name = "hp_server."+serial
+
+    # Create new directory tree for server, copy from template
+    source_dir = "./hp_server.template"
+    dest_dir = "./instances/"+instance_name
+    shutil.copytree(source_dir, dest_dir)
+
+    # Replace unique identifiers (serial, hostname, server_name, ip) in server directory tree
+    findReplace("./hp_server_test", "{{SERIAL_NUMBER}}", serial, "*")
+    findReplace("./hp_server_test", "{{ILO_HOSTNAME}}", hostname, "*")
+    findReplace("./hp_server_test", "{{SERVER_NAME}}", server_name, "*")
+    findReplace("./hp_server_test", "{{IP_ADDRESS}}", ip, "*")
+
+client = docker.from_env()
+container = client.containers.run('michzimm/redfish_sim', volumes=['./instances/'+instance_name])
